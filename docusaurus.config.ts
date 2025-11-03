36f789efd4f58917c845a55d7f24ba65830fd9d7
seoyoung-dev <seoyoung.dev@gmail.com>
2025-11-04 00:08:49 +0900

    feat: configure OpenAPI docs plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,6 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as OpenApiPlugin from 'docusaurus-plugin-openapi-docs';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -35,38 +36,90 @@ const config: Config = {
     locales: ['en'],
   },
 
+  // presets: [
+  //   [
+  //     'classic',
+  //     {
+  //       docs: {
+  //         sidebarPath: './sidebars.ts',
+  //         // Please change this to your repo.
+  //         // Remove this to remove the "edit this page" links.
+  //         editUrl:
+  //           'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+  //       },
+  //       blog: {
+  //         showReadingTime: true,
+  //         feedOptions: {
+  //           type: ['rss', 'atom'],
+  //           xslt: true,
+  //         },
+  //         // Please change this to your repo.
+  //         // Remove this to remove the "edit this page" links.
+  //         editUrl:
+  //           'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+  //         // Useful options to enforce blogging best practices
+  //         onInlineTags: 'warn',
+  //         onInlineAuthors: 'warn',
+  //         onUntruncatedBlogPosts: 'warn',
+  //       },
+  //       theme: {
+  //         customCss: './src/css/custom.css',
+  //       },
+  //     } satisfies Preset.Options,
+  //   ],
+  // ],
+
   presets: [
     [
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/PaloAltoNetworks/docusaurus-openapi-docs/tree/main/demo',
+          docItemComponent: '@theme/ApiItem', // Derived from docusaurus-theme-openapi
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
+        },
+        gtag: {
+          trackingID: 'GTM-THVM29S',
+          anonymizeIP: false,
         },
       } satisfies Preset.Options,
     ],
   ],
+  plugins: [
+    [
+      'docusaurus-plugin-openapi-docs',
+      {
+        id: 'openapi', // plugin id
+        docsPluginId: 'classic', // configured for preset-classic
+        config: {
+          petstore: {
+            specPath: 'static/openapi/petstore.yaml',
+            proxy: 'https://cors.pan.dev',
+            outputDir: 'docs/api/petstore',
+            sidebarOptions: {
+              groupPathsBy: 'tag',
+              categoryLinkSource: 'tag',
+            },
+            template: 'static/templates/api.mustache',
+            infoTemplate: 'static/templates/info.mustache',
+            tagTemplate: 'static/templates/tag.mustache',
+            schemaTemplate: 'static/templates/schema.mustache',
+            downloadUrl: '/petstore.yaml',
+            hideSendButton: false,
+            showSchemas: true,
+          } satisfies OpenApiPlugin.Options,
+        },
+      },
+    ],
+  ],
+
+  themes: ['docusaurus-theme-openapi-docs'],
 
   themeConfig: {
     // Replace with your project's social card
@@ -87,58 +140,24 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
           position: 'right',
         },
+        {
+          type: 'docSidebar',
+          sidebarId: 'openapiSidebar',
+          position: 'left',
+          label: 'API sample',
+        },
       ],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} seoyoung-dev, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
